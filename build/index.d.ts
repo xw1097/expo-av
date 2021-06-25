9b20d814882a901c24950de0c593286a79c8c194
Xicheng Wang <xw1097@gmail.com>
2021-06-24 22:46:48 -0700

    Empty folder

--- build/index.d.ts
+++
@@ -1,3 +0,0 @@
-import * as Audio from './Audio';
-export { Audio };
-export { default as Video, VideoProps, VideoState, AVInterstitial, AVPlaybackNativeSource, AVPlaybackStatus, AVPlaybackStatusToSet, VideoNativeProps, VideoNaturalSize, VideoReadyForDisplayEvent, ResizeMode, VideoFullscreenUpdateEvent, } from './Video';
